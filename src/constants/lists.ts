4ad730dc9188f0e942cbdf3e851ca68e4db5626c
gogochro <chro_official@outlook.com>
2025-03-31 03:03:08 +0800

    v1

---
+++ src/constants/lists.ts
@@ -0,0 +1,7 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/chro-dev/chroswap/main/src/constants/Token.json'
+// the Uniswap Default token list lives here
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL
+]
